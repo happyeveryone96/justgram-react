7258c32b5e82d2c3d675ebec8b07eeabf22d2d2a
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-05-22 21:53:37 +0900

    Add: 메인 페이지 추가

--- just-gram-react/src/App.js
+++ just-gram-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './pages/Login/Login.js';
+import Main from './pages/Main/Main.js';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <Main/>
     </div>
   );
 }
