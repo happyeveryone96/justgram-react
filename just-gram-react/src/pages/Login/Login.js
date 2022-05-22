48e44758c68367d5e0c48146bfd7a4eb5d94ecd7
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-05-22 18:54:11 +0900

    Add: 로그인 페이지 추가

---
+++ just-gram-react/src/pages/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Login.css';
+
+function Login() {
+  return (
+      <div className="container">
+        <h1>Justgram</h1>
+        <input type="text" id="id-input" placeholder="전화번호, 사용자 이름 또는 이메일"/>
+        <input type="password" id="pw-input" placeholder="비밀번호"/>
+        <button id="login-btn">로그인</button>
+        <a href="/" id="search-pw">비밀번호를 잊으셨나요?</a>
+      </div>
+  )
+}
+
+export default Login;
