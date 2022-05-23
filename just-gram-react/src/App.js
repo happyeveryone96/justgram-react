8fc29a316b5943975396e9b5847c5809fad57747
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-05-23 14:49:35 +0900

    Add: 라우팅 기능 추가

--- just-gram-react/src/App.js
+++ just-gram-react/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Login from './pages/Login/Login.js';
-import Main from './pages/Main/Main.js';
+import Router from './components/Router';
 
 function App() {
   return (
     <div className="App">
-      {/* <Login/> */}
-      <Main/>
+      <Router/>
     </div>
   );
 }
--- just-gram-react/src/pages/Login/Login.js
+++ just-gram-react/src/pages/Login/Login.js
@@ -5,9 +5,11 @@ function Login() {
   return (
       <div className="container">
         <h1>Justgram</h1>
-        <input type="text" id="id-input" placeholder="전화번호, 사용자 이름 또는 이메일"/>
-        <input type="password" id="pw-input" placeholder="비밀번호"/>
-        <button id="login-btn">로그인</button>
+        <div className="login-form">
+          <input type="text" id="id-input" placeholder="전화번호, 사용자 이름 또는 이메일"/>
+          <input type="password" id="pw-input" placeholder="비밀번호"/>
+          <button id="login-btn">로그인</button>
+        </div>
         <a href="/" id="search-pw">비밀번호를 잊으셨나요?</a>
       </div>
   )
