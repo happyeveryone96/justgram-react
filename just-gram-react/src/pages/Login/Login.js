e02a5aeab20d80fdbe0c27876062cb8f040f1504
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-05-23 15:21:53 +0900

    Add: 로그인 버튼 클릭 시 메인 페이지로 이동 기능 추가

--- just-gram-react/src/pages/Login/Login.js
+++ just-gram-react/src/pages/Login/Login.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import './Login.css';
 
 function Login() {
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate("/main");
+  };
   return (
       <div className="container">
         <h1>Justgram</h1>
         <div className="login-form">
           <input type="text" id="id-input" placeholder="전화번호, 사용자 이름 또는 이메일"/>
           <input type="password" id="pw-input" placeholder="비밀번호"/>
-          <button id="login-btn">로그인</button>
+          <button onClick={goToMain}>로그인</button>
         </div>
         <a href="/" id="search-pw">비밀번호를 잊으셨나요?</a>
       </div>
