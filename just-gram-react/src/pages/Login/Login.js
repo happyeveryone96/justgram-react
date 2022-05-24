df8a3f1a3f75701dc735aacbbc52753e0649601b
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-05-24 13:07:21 +0900

    Add: 로그인 기능 활성화 기능 추가

--- just-gram-react/src/pages/Login/Login.js
+++ just-gram-react/src/pages/Login/Login.js
@@ -6,6 +6,15 @@ import './Login.scss';
 function Login() {
   const [idValue, setIdValue] = useState('');
   const [pwValue, setPwValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const updateBtn = () => {
+    if (idValue.includes('@') && pwValue.length >= 5) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }
 
   const handleIdInput = (e) => {
     setIdValue(e.target.value);
@@ -24,9 +33,16 @@ function Login() {
       <div className="container">
         <h1>Justgram</h1>
         <div className="login-form">
-          <input type="text" id="id-input" onChange={handleIdInput} placeholder="전화번호, 사용자 이름 또는 이메일"/>
-          <input type="password" id="pw-input" onChange={handlePwInput} placeholder="비밀번호"/>
-          <button onClick={goToMain}>로그인</button>
+          <input type="text" id="id-input" onKeyUp={updateBtn} onChange={handleIdInput} placeholder="전화번호, 사용자 이름 또는 이메일"/>
+          <input type="password" id="pw-input" onKeyUp={updateBtn} onChange={handlePwInput} placeholder="비밀번호"/>
+          <button 
+            disabled={isValid ? false : true} 
+            style={{
+              backgroundColor: isValid ? 'rgb(115,180,226)': 'rgb(172,212,234)', 
+              cursor: isValid ? 'pointer' : 'auto'}} 
+            onClick={goToMain}>
+              로그인
+          </button>
         </div>
         <a href="/" id="search-pw">비밀번호를 잊으셨나요?</a>
       </div>
