96db3b8f38dfbed821780a2a61cc08e6d36ba8c3
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-05-24 10:09:08 +0900

    Add: 사용자 입력 데이터 저장 기능 구현

--- just-gram-react/src/pages/Login/Login.js
+++ just-gram-react/src/pages/Login/Login.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import './Login.scss';
 
 function Login() {
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
+
+  const handleIdInput = (e) => {
+    setIdValue(e.target.value);
+  }
+
+  const handlePwInput = (e) => {
+    setPwValue(e.target.value);
+  }
+
   const navigate = useNavigate();
 
   const goToMain = () => {
@@ -13,8 +24,8 @@ function Login() {
       <div className="container">
         <h1>Justgram</h1>
         <div className="login-form">
-          <input type="text" id="id-input" placeholder="전화번호, 사용자 이름 또는 이메일"/>
-          <input type="password" id="pw-input" placeholder="비밀번호"/>
+          <input type="text" id="id-input" onChange={handleIdInput} placeholder="전화번호, 사용자 이름 또는 이메일"/>
+          <input type="password" id="pw-input" onChange={handlePwInput} placeholder="비밀번호"/>
           <button onClick={goToMain}>로그인</button>
         </div>
         <a href="/" id="search-pw">비밀번호를 잊으셨나요?</a>
